00f2fa83775c3cc24044182c3ce41c830e96a39a
dhruvermafz <vermadhruv09112002@gmail.com>
2023-10-19 12:20:29 +0530

    registrion form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { BrowserRouter } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features.jsx';
-import Testimonial from './components/Testimonial';
-import CallToAction from './components/CallToAction';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features.jsx";
+import Testimonial from "./components/Testimonial";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
+import Register from "./components/Auth/Register";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
       <Navbar />
       <Hero />
       <Features />
       <Testimonial />
       <CallToAction />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
